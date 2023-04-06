8caa9b9d557995b5b3d1ab677fb6973589aba17e
jrgleason <jackiegleason@gmail.com>
2023-04-06 14:57:44 -0400

    The site is looking better

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,52 @@
-import '@/styles/Header.module.scss'
+import styles from '@/styles/Header.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMenorah } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Header = () => {
+    function handleResumeClick() {
+        window.location.href = 'https://docs.google.com/document/d/1xrqve90hloOYGR3RSCvwXJBXq_NQR5kmNfrxKYSveKk';
+    }
     return (
         <nav className="
-            bg-black
+            bg-transparent
             text-white
             mx-auto
             flex
             items-center
             justify-between
+            absolute
+            w-full
+            top-0
+            left:0
             p-6
+            z-50
             lg:px-8"
              aria-label="Global">
-            <FontAwesomeIcon icon={faMenorah} />
-            <div>MySite</div>
+            <FontAwesomeIcon icon={faBars} />
+            <div className="jrg-header-title flex flex-1 justify-center text-xl">
+                <strong>THE&nbsp;</strong>
+                <span> Jackie R. Gleason</span>
+            </div>
+            <div className= {`flex justify-between justify-end ${ styles["jrg-buttons"]}`}>
+                <button
+                    className="
+                        jrg-resume-button
+                        rounded
+                        border-transparent
+                        capitalize
+                    "
+                    onClick={handleResumeClick}
+                >
+                    resumé
+                </button>
+                <a href="https://www.linkedin.com/in/jackie-gleason-5a52114/" slot="actionItems">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="https://github.com/jrgleason" slot="actionItems">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </div>
+
         </nav>
     );
 };
